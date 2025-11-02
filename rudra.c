6015a617469980e6900726110468d5343a6d2379
Rudra Mina <rudraminaa@gmail.com>
2025-11-02 20:21:51 +0530

    Added a new variable name

--- rudra.c
+++ rudra.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+    char* name = "Rudra";
     printf("This is the main branch");
-    printf("This is a new feature");
+    printf("This is a new feature %s",name);
 }
